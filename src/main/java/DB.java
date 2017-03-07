eda89698c4f2f4a61be5df6092de2ba7683906a3
brianmituka <mitukab@outlook.com>
2017-03-07 07:52:48 +0300

    done

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,5 @@
 import org.sql2o.*;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/drug_store", "vagabond", "472113");
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/drug_store", "mots", "koldenod19*");
 }
